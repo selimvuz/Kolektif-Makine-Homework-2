bf0499077872beae9e67285cdf4c7a375c361e1d
Yavuz Selim <96388835+selimvuz@users.noreply.github.com>
2023-11-27 22:54:12 +0300

    Input sample here

--- input.py
+++ input.py
@@ -6,22 +6,22 @@ from transformers import BertTokenizer, TFBertForSequenceClassification
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Number of models
+# Toplam model sayısı
 num_models = 5
 
-# Load pre-trained BERT model and tokenizer
+# Önceden eğitilmiş BERT modelini ve tokenizer'ı yükleyin
 tokenizer = BertTokenizer.from_pretrained("Models/model_1")
 
-# Load the fine-tuned models
+# Fine-tune edilmiş modelleri yükleyin
 models = []
 for i in range(1, num_models + 1):
     model = TFBertForSequenceClassification.from_pretrained(
         f"Models/model_{i}")
     models.append(model)
 
-# Load and preprocess a sample
+# Bir örnek metin ve etiket oluşturun
 max_length = 128
-# Replace with your actual sample text
+# Gerçek örneğinizi girin
 sample_text = "hiç beğenmedim çok kötüydü 1 yıldız bile fazla"
 sample_encodings = tokenizer([sample_text], truncation=True,
                              padding=True, max_length=max_length, return_tensors="tf")
@@ -39,7 +39,7 @@ def convert_to_hashable(batch_encoding):
 hashable_encodings = convert_to_hashable(sample_encodings)
 tensor_encodings = convert_to_tensors(hashable_encodings)
 
-# Display each model's prediction and actual value
+# Her modelin tahminlerini ve gerçek değerleri yazdırın
 for i, model in enumerate(models):
     model_prediction = model.predict(tensor_encodings)
     model_prediction_label = np.argmax(model_prediction["logits"])
@@ -47,16 +47,16 @@ for i, model in enumerate(models):
     print(
         f"Model {i + 1} - Tahmini Değer: {model_prediction_label}, Gerçek Değer: {sample_label}")
 
-# Ensemble prediction by averaging
+# Averaging yöntemiyle ensemble tahmini yapın
 ensemble_predictions = np.zeros((1, num_models))
 
 for i, model in enumerate(models):
     model_prediction = model.predict(tensor_encodings)
     ensemble_predictions[0, i] = tf.argmax(model_prediction["logits"], axis=1)
 
-# Calculate the ensemble result by averaging predictions
+# Sonucu hesapla
 ensemble_prediction_avg = int(np.round(np.mean(ensemble_predictions)))
 
-# Display ensemble prediction
+# Tahmini yazdır
 print(
     f"\nEnsemble Tahmini (Averaging): {ensemble_prediction_avg}, Gerçek Değer: {sample_label}")
