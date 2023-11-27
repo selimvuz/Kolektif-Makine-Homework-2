6142ebb48efd9f3c9f1193974079e070d350321f
Yavuz Selim <96388835+selimvuz@users.noreply.github.com>
2023-11-27 22:48:16 +0300

    Bootstrap resampling

--- bootstrap_resampling.py
+++ bootstrap_resampling.py
@@ -1,15 +1,18 @@
 import numpy as np
 
-# Original dataset
+# Bu kod, bootstrap resampling yönteminin nasıl uygulandığını
+# basit bir şekilde göstermektedir.
+
+# Orijinal veri kümesi
 original_data = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-# Number of bootstrap samples
+# Bootstrap örneklemlerinin sayısı
 num_bootstrap_samples = 5
 
-# Generate bootstrap samples
+# Bootstrap örneklemlerini oluştur
 bootstrap_samples = [np.random.choice(original_data, size=len(
     original_data), replace=True) for _ in range(num_bootstrap_samples)]
 
-# Print the bootstrap samples
+# Bootstrap örneklemlerini yazdır
 for i, sample in enumerate(bootstrap_samples):
     print(f"Bootstrap Sample {i + 1}: {sample}")
