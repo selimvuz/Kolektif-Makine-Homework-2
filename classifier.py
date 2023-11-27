21eff0c4e785dfadd194d7abcc60e8b51bee6ca4
Yavuz Selim <96388835+selimvuz@users.noreply.github.com>
2023-11-27 22:50:49 +0300

    Classifier Training

--- classifier.py
+++ classifier.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, Gradien
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC, LinearSVC
 
-# Load data
+# Verileri yükle
 data = np.load('combined_feature_vectors.npy')
 X = data[:, :-1]  # Feature vectors
 y = data[:, -1]   # Star ratings
@@ -17,32 +17,32 @@ bins = [0, 1, 2, 3, 4, 5]  # Define the bin edges
 labels = ['Low', 'Low-Med', 'Medium', 'Med-High',
           'High']  # Assign labels to the bins
 
-# Create a new column with discrete categories
+# Kesikli kategorileri içeren yeni bir sütun oluşturun
 y_discrete = pd.cut(y, bins=bins, labels=labels, include_lowest=True)
 
-# Map labels to numerical values
+# Alanları sayısal değerlere eşleyin
 label_mapping = {'Low': 1, 'Low-Med': 2, 'Medium': 3, 'Med-High': 4, 'High': 5}
 y_discrete_numeric = y_discrete.map(label_mapping)
 
-# Split the data into training and testing sets
+# Veriyi eğitim ve test setlerine ayırın
 X_train, X_test, y_train, y_test = train_test_split(
     X, y_discrete_numeric, test_size=0.2, random_state=42)
 
-# Create a logistic regression model
+# Ridge Classifier
 model = RidgeClassifier()
 
-# Train the model
+# Modeli eğitin
 model.fit(X_train, y_train)
 
-# Make predictions on the test set
+# Test veri setini kullanarak modeli değerlendirin
 y_pred = model.predict(X_test)
 
-# Evaluate the model
+# Modelin performansını değerlendirin
 accuracy = accuracy_score(y_test, y_pred)
 report = classification_report(y_test, y_pred)
 
 print(f"Accuracy: {accuracy}")
 print("Classification Report:\n", report)
 
-# Save the trained model to a file
+# Eğitilen modeli kaydedin
 joblib.dump(model, 'Classifiers/RidgeClassifier.joblib')
