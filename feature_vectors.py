44da45261abe2377db1b979b8d1adcbf9d332ca2
Yavuz Selim <96388835+selimvuz@users.noreply.github.com>
2023-11-27 22:52:20 +0300

    Feature Vectors

--- feature_vectors.py
+++ feature_vectors.py
@@ -3,13 +3,13 @@ import numpy as np
 import pandas as pd
 import json
 
-# Load your JSON data directly
+# JSON verilerini doğrudan yükleyin
 dataset = pd.read_json('Datasets/part_0.json')
 
 tokenizer = BertTokenizer.from_pretrained('dbmdz/bert-base-turkish-uncased')
 model = TFBertModel.from_pretrained('dbmdz/bert-base-turkish-uncased')
 
-# Initialize lists to store feature vectors
+# Öznitelik vektörlerini depolamak için bir liste oluşturun
 feature_vectors = []
 max_sequence_length = 512
 
@@ -17,13 +17,13 @@ for index, row in dataset.iterrows():
     text = row['text'][:max_sequence_length - 2]
     star_rating = row['star']
 
-    # Tokenize and encode the text
+    # Metni tokenize edin ve modeli kullanarak gömme vektörünü alın
     inputs = tokenizer(text, return_tensors="tf", truncation=True,
                        max_length=max_sequence_length, padding='max_length')
     outputs = model(**inputs)
     embeddings = outputs.last_hidden_state.numpy()
 
-    # Combine the text embeddings with the star rating
+    # Metin gömme vektörlerini ortalayın ve yıldız derecelendirmesini ekleyin
     combined_vector = np.concatenate(
         [embeddings.mean(axis=1).squeeze(), np.array([star_rating])])
 
